5a406463b610bdb77732d7ebb1bf72fe7fdc5bc5
striker-14 <pranaypartapsingh14@gmail.com>
2026-09-18 05:19:04 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,18 +2,13 @@
 using namespace std;
 
 void pattern(int n) {
-    for (int i = 0; i < n; i++) {
-
-        for (int j = 0; j <= i; j++) {
-            cout << "*";
-        }
-
-        cout << endl;
-    }
-
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1-i; j++) {
-            cout << "*";
+    for (int i = 0; i < 2*n-1; i++) {
+        for (int j = 0; j < 2*n-1; j++) {
+            int top = i;
+            int left = j;
+            int right = (2*n-2) - j;
+            int bottom = (2*n-2) - i;
+            cout << n - min(min(top, bottom), min(left, right));
         }
         cout << endl;
     }
